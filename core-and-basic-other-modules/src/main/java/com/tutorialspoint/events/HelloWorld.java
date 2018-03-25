783336e8448ec1afd54635e5196dc4e6713382e9
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-03-25 11:57:32 +0300

    Example of AppConetxt's events

---
+++ core-and-basic-other-modules/src/main/java/com/tutorialspoint/events/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.tutorialspoint.events;
+
+public class HelloWorld {
+	
+	private String message;
+
+	public String getMessage() {
+		return "Your message : " + message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ core-and-basic-other-modules/src/main/java/com/tutorialspoint/events/MainApp.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint.events;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+	ConfigurableApplicationContext context = 
+	new ClassPathXmlApplicationContext("Beans.xml");
+	
+	// Let us raise a start event.
+	context.start();
+		    		  
+	HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	obj.setMessage("You're in context's life cycle");
+	obj.getMessage();
+	
+	// Let us raise a stop event.
+	context.stop();
+	}
+
+}

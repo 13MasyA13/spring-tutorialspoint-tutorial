8be00cad2050ebcd989c67dace343ac5f0378e27
Max Golik <maximko1312@gmail.com>
2018-03-28 15:04:40 +0300

    Example of aop:after advice

--- spring-aop/src/main/java/com/tutorialspoint/xml/Logging.java
+++ spring-aop/src/main/java/com/tutorialspoint/xml/Logging.java
@@ -2,11 +2,7 @@ package com.tutorialspoint.xml;
 
 public class Logging {
 	
-	   /** 
-	    * This is the method which I would like to execute
-	    * before a selected method execution.
-	    */
-	   public void beforeAdvice() {
-	      System.out.println("Going to setup student profile.");
-	   }
+	public void afterAdvice(){
+	      System.out.println("Student profile setup complete.");
+	   }  
 }
